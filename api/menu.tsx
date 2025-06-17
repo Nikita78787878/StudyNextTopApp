7dacf111c4438c28287e88f96c9da59f9edff50e
Epov Nikita <Nikita.Epov@develonica.ru>
2025-06-17 18:24:03 +0800

    Посмотрели работу SSR и SSG

---
+++ api/menu.tsx
@@ -0,0 +1,13 @@
+import {MenuItem} from "@/interfaces/menu.interface";
+import {API} from "@/app/api";
+
+export async function getMenu(firstCategory: number): Promise<MenuItem[]> {
+    const res = await fetch(API.topPage.find, {
+        method: 'POST',
+        body: JSON.stringify({firstCategory}),
+        headers: new Headers({'content-type': 'application/json'}),
+        next: { revalidate: 10 } // чтобы из статичной страницы получаить динамическую
+    });
+    console.log('revalidating getMenu'); // чисто увидеть что динамически меняется
+    return res.json();
+}
---
+++ api/page.tsx
@@ -0,0 +1,14 @@
+import {API} from "@/app/api";
+import {TopPageModel} from "@/interfaces/page.interface";
+
+export async function getPage(alias: string): Promise<TopPageModel | null> {
+    const res = await fetch(API.topPage.byAlias + alias, {
+        next: { revalidate: 10 } // чтобы из статичной страницы получаить динамическую
+    });
+    console.log('revalidating getPage');// чисто увидеть что динамически меняется
+
+    if (!res.ok) {
+        return null;
+    }
+    return res.json();
+}
